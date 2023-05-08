83e5befbe0a51ffef5232a2d7038940cfd56fdeb
DonMatano <mwakimamatano@gmail.com>
2023-05-08 15:36:14 +0300

    Show modal

--- components/ImageUploader.client.vue
+++ components/ImageUploader.client.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="my-4">
-    <Dashboard :uppy="uppy" :plugins="['ImageEditor']" />
+    <DashboardModal :uppy="uppy" :plugins="['ImageEditor']" :open="props.showModal"  />
   </div>
 </template>
 
 <script setup lang="ts">
-import {Dashboard} from '@uppy/vue';
+import {DashboardModal} from '@uppy/vue';
 import Uppy from '@uppy/core';
 import ImageEditor from '@uppy/image-editor';
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 import '@uppy/image-editor/dist/style.min.css';
+
+const props = defineProps({
+  showModal: {type: Boolean, default: false}
+})
 
 const uppy = new Uppy().use(ImageEditor, {quality: 0.8});
 
---
+++ pages/projects/tengeneza.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-body">
+    <h2 class="text-[2.5rem] md:text-[4.5rem] lg:text-xl font-bold my-8">
+      Create Project
+    </h2>
+
+    <form class="flex flex-col gap-4">
+      <input type="text" placeholder="Project Name"
+        class="bg-transparent border-b border-white py-3 px-4 outline-none focus:border-accent-teal" />
+      <textarea row="10" maxlength="250" placeholder="Quick project description"
+        class="bg-transparent border-b border-white py-3 px-4 outline-none focus:border-accent-teal" />
+      <div class="flex flex-col">
+        <Editor />
+      </div>
+      <button type="button" class="justify-end px-4 py-1 border border-accent-teal" 
+        @click="showModal"
+      >
+        Upload project cover page
+      </button>
+      <ImageUploader :showModal="isShowingUploadCoverPageModal" />
+      <input type="text" placeholder="Techs Used"
+        class="bg-transparent border-b border-white py-3 px-4 outline-none focus:border-accent-teal" />
+      <button type="button"
+        class="self-end border-b border-accent-teal py-2  leading-[1.625rem] tracking-[0.14em] font-bold hover:text-accent-teal">CREATE</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const isShowingUploadCoverPageModal = ref(false);
+function showModal() {
+  isShowingUploadCoverPageModal.value = !isShowingUploadCoverPageModal.value;
+}
+</script>
